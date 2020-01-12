fdce4dbd884c7416e0f27f948e10b211bfb4f559
illusi0n <milosbiljanovic@gmail.com>
2020-01-12 17:09:25 +0100

    Added template engine for converting match results to html

---
+++ ScoreBoardEmailGeneratorFunction/src/main/java/com/thelivelock/email_generator/scoreboard/Person.java
@@ -0,0 +1,13 @@
+package com.thelivelock.email_generator.scoreboard;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ ScoreBoardEmailGeneratorFunction/src/main/java/com/thelivelock/email_generator/scoreboard/model/Team.java
@@ -0,0 +1,22 @@
+package com.thelivelock.email_generator.scoreboard.model;
+
+public class Team {
+    private String name;
+    private String logoUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+}
